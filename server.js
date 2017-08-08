d43e78a557e5e57ce632a22d580eb1e9217f436b
sneusneu22 <30697832+sneusneu22@users.noreply.github.com>
2017-08-08 13:32:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/article-one', function (req,res) {
 });
 
 app.get('/article-two', function (req,res) {
-   res.send('Article two requested and will be served here'); 
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-three', function (req,res) {
